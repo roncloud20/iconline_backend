b5a939798137967054a37f75fe9455eb9369abdc
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2024-06-25 13:04:44 +0100

    creating and configure the UserController and api file

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // creating the staff registrition controller
+    function staffRegistration(Request $request){
+        return "Hello";
+    }
+
+
+}
